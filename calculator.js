5daba2526d23ed90daeecf5544b87fb214782ee7
Amusenna <ahmet@visiosoft.com.tr>
2022-06-21 13:25:10 +0200

    Last chaces

--- calculator.js
+++ calculator.js
@@ -8,12 +8,13 @@ const port = 3000
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/index.html")
 });
 
 app.post("/", function(req, res) {
-
 
   var selectedOne =Number(req.body.num1);
   var selectedTwo =Number(req.body.num2);
@@ -21,6 +22,24 @@ app.post("/", function(req, res) {
   var result = selectedOne + selectedTwo
 
 res.send("Thanks for make an calculate your result is " + result);
+
+});
+
+
+
+
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + "/bmicalculator.html")
+});
+
+app.post("/bmicalculator", function(req, res) {
+
+  var weight =parseFloat(req.body.weight1);
+  var height =parseFloat(req.body.height1);
+
+  var bmi = weight / ( height * height );
+
+res.send("Thanks for make an BMI calculate your BMI is " + bmi);
 
 });
 
